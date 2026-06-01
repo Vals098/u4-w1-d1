1298cca9112541d162cbab9f1ed4e80b74b57a8c
Vals098 <valeria.farinosi@gmail.com>
2026-06-01 16:35:44 +0200

    add: Es2

--- src/Es2.java
+++ src/Es2.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Es2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First string:");
+        String str1 = scanner.nextLine();
+
+        System.out.println("Second string:");
+        String str2 = scanner.nextLine();
+
+        System.out.println("Third string:");
+        String str3 = scanner.nextLine();
+
+        String concatInOrder = str1 + str2 + str3;
+        System.out.println(concatInOrder);
+
+        String concatInInverseOrder = str3 + str2 + str1;
+        System.out.println(concatInInverseOrder);
+    }
+
 }
