2e22d175d48f548500696d70c8df27ec2a1fad61
Vals098 <valeria.farinosi@gmail.com>
2026-06-01 16:58:32 +0200

    add: Es 3

---
+++ src/Es3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Es3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input width:");
+        int width = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Input height:");
+        int height = Integer.parseInt(scanner.nextLine());
+
+
+        int result = perimeter(width, height);
+        System.out.println(result);
+
+
+    }
+
+    public static int perimeter(int width, int height) {
+        return (width + height) * 2;
+    }
+}
